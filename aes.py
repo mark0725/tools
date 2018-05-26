bda909271e3097d2688098d3339f8a8f291a0184
mark0725 <gaoxilin@gmail.com>
2018-05-26 11:39:19 +0800

    key support other lenght

--- aes.py
+++ aes.py
@@ -1,10 +1,18 @@
 #coding: utf8  
+#example: python aes.py keyskeyskeyskeys 0123456789ABCDEF
 import sys  
 from Crypto.Cipher import AES  
 from binascii import b2a_hex, a2b_hex  
    
 class aestool():  
     def __init__(self, key):  
+        count = len(key) 
+        if(count % 16 != 0) :  
+            add = 16 - (count % 16)  
+        else:  
+            add = 0  
+
+        key = key + ('\0' * add)
         self.key = key  
         self.mode = AES.MODE_CBC  
        
@@ -31,12 +39,22 @@ class aestool():
         plain_text = cryptor.decrypt(a2b_hex(text))  
         return plain_text.rstrip('\0')  
    
-if __name__ == '__main__':  
-    pc = aestool('keyskeyskeyskeys')      #初始化密钥  
-    e = pc.encrypt("0123456789ABCDEF")  
-    d = pc.decrypt(e)                       
-    print e, d  
+def main(argv):
 
-    e = pc.encrypt("00000000000000000000000000")  
-    d = pc.decrypt(e)                    
-    print e, d  
+    if len(argv) < 3 :
+        print 'Useage: ', argv[0], ' key data'
+        print '  key: length 16 bytes.'
+        sys.exit(1)
+
+    if len(argv[1]) > 16 :
+        print '  key max length 16 bytes.'
+        sys.exit(1)
+
+    pc = aestool(argv[1])      #初始化密钥  
+    e = pc.encrypt(argv[2])  
+    d = pc.decrypt(e)                       
+    print 'plain text:', d
+    print 'encrypt text:', e
+
+if __name__ == '__main__':
+    main(sys.argv) 
